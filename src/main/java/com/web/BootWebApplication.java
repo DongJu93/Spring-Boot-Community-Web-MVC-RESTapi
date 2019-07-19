8f3bc3136b5d09d78984521e2641b8d8b8ad64e0
DongJu <spdlqj011@gmail.com>
2019-07-19 13:07:44 +0900

    ADMIN user "havi"

--- src/main/java/com/web/BootWebApplication.java
+++ src/main/java/com/web/BootWebApplication.java
@@ -35,29 +35,29 @@ public class BootWebApplication extends WebMvcConfigurerAdapter {
 		argumentResolvers.add(userArgumentResolver);
 	}
 
-//	@Bean
-//	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
-//		return (args) -> {
-//			User user = userRepository.save(User.builder()
-//					.name("havi")
-//					.password("test")
-//					.email("havi@gamil.com")
-//					.createdDate(LocalDateTime.now())
-//					.build()
-//			);
-//
-//			IntStream.rangeClosed(1, 200).forEach(index ->
-//					boardRepository.save(Board.builder()
-//							.title("게시글"+index)
-//							.subTitle("순서"+index)
-//							.content("콘텐츠")
-//							.boardType(BoardType.free)
-//							.createdDate(LocalDateTime.now())
-//							.updatedDate(LocalDateTime.now())
-//							.user(user)
-//							.build()
-//					)
-//			);
-//		};
-//	}
+	/*@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
+		return (args) -> {
+			User user = userRepository.save(User.builder()
+					.name("havi")
+					.password("test")
+					.email("havi@gamil.com")
+					.createdDate(LocalDateTime.now())
+					.build()
+			);
+
+			IntStream.rangeClosed(1, 200).forEach(index ->
+					boardRepository.save(Board.builder()
+							.title("게시글"+index)
+							.subTitle("순서"+index)
+							.content("콘텐츠")
+							.boardType(BoardType.free)
+							.createdDate(LocalDateTime.now())
+							.updatedDate(LocalDateTime.now())
+							.user(user)
+							.build()
+					)
+			);
+		};
+	}*/
 }
